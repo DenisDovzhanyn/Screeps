78ccba5085ef3b563b5a8125305c28a4bfadeee9
Denis <dovzhanyndenis03@gmail.com>
2024-07-14 01:47:24 -0400

    harvesters now switch between extensions and spawn

--- role.harvester.js
+++ role.harvester.js
@@ -1,13 +1,22 @@
 var roleHarvester = {
      run: function(creep) {
+         
+         var structs = creep.room.find(FIND_STRUCTURES, {
+             filter: (structure) => {
+                 return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) 
+                 && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+             }
+         })
+         
          if(creep.store.getFreeCapacity() > 0){
              var sources = creep.room.find(FIND_SOURCES)
              if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE){
                  creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ff0000'}})
              }
-         } else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity){
-             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                 creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ff0000'}})
+         } 
+         else if(structs.length > 0){
+             if(creep.transfer(structs[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                 creep.moveTo(structs[0], {visualizePathStyle: {stroke: '#ff0000'}})
              }
          } else{
              creep.moveTo(Game.flags.AFK, {visualizePathStyle: {stroke: '#ff0000'}})
